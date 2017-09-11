8cb0cd98a6e405b19b63d9a0b77169cd2cffb7bf
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-09-11 16:01:35 +0300

    publishing deck

--- sentiments.R
+++ sentiments.R
@@ -1,3 +1,5 @@
+Sys.setlocale(,"UK_ua")
+
 library(sentimentr)
 library(tidyverse)
 library(tidytext)
@@ -5,7 +7,7 @@ senti_words <- data_frame(term=unique(c(sent_h$x,sent_j$x,sent_n$x,sent_s$x,lexi
                                         SentimentAnalysis::DictionaryGI$positive,SentimentAnalysis::DictionaryHE$negative,SentimentAnalysis::DictionaryHE$positive,
                                         SentimentAnalysis::DictionaryLM$negative,SentimentAnalysis::DictionaryLM$positive,SentimentAnalysis::DictionaryLM$uncertainty)))
 
-senti_words <- readxl::read_excel("~/infovis/senti_words.xlsx")
+senti_words <- readxl::read_excel("senti_words.xlsx")
 
 lexicon::hash_sentiment_huliu
 lexicon::hash_emoticons
@@ -29,44 +31,66 @@ masiv <- read_csv("masiv.csv")
 
 tonal <- masiv$title_ru %>% 
   as_data_frame() %>% 
-  mutate(id_text=c(1:12059))  %>%  
+  mutate(id_text=c(1:12059),nwords=stringr::str_count(value,"\\S+"))  %>%  
   unnest_tokens(sentence,value,token = "sentences") %>% 
   group_by(id_text) %>% 
-  mutate(id=c(1:n()),nwords=stringr::str_count(sentence,"\\S+")) %>% 
-  unnest_tokens(words,sentence,token = "skip_ngrams", n = 2) %>% 
-  left_join(sentiword_ua_ru,by="words") %>% 
+  mutate(id=c(1:n())) %>% 
+  unnest_tokens(words,sentence,token = "ngrams", n = 3) %>% 
+  separate(words,c("word1","word2","word3"),sep = " ") %>% 
+  left_join(sentiword_ua_ru,by=c("word1"="words")) %>% 
+  left_join(sentiword_ua_ru,by=c("word2"="words")) %>% 
+  left_join(sentiword_ua_ru,by=c("word3"="words")) %>% 
+  filter(!is.na(polarity.x)) %>% 
+  mutate(polarity=polarity.x*ifelse(!is.na(polarity.y),polarity.y,1)*ifelse(!is.na(polarity.y)&!is.na(polarity),
+                                                                            polarity,1)) %>% 
+  select(id_text,id,nwords,word1, word2, word3, polarity) %>% 
   group_by(id_text) %>% 
-  summarize(nwords = sum(nwords,na.rm=T), polarity=sum(polarity, na.rm=T)/(nwords+1)) %>% 
-  mutate(polarity=(polarity - mean(polarity)) / sd(polarity))
+  summarize(nwords = mean(nwords,na.rm=T), polarity=sum(polarity, na.rm=T)/(nwords+1)) #%>% 
+  #mutate(polarity=ifelse(polarity>0,scales::rescale(polarity,to=c(0,1)),
+  #                       ifelse(polarity<0,scales::rescale(polarity,to=c(0,-1)),0
+  #                       )))
 
 tidysent <- function(x){
   x %>% 
     as_data_frame() %>% 
-    mutate(id_text=c(1:12059))  %>%  
+    mutate(id_text=c(1:12059),nwords=stringr::str_count(value,"\\S+"))  %>%  
     unnest_tokens(sentence,value,token = "sentences") %>% 
     group_by(id_text) %>% 
-    mutate(id=c(1:n()),nwords=stringr::str_count(sentence,"\\S+")) %>% 
-    unnest_tokens(words,sentence) %>% 
-    left_join(sentiword_ua_ru,by="words") %>% 
+    mutate(id=c(1:n())) %>% 
+    unnest_tokens(words,sentence,token = "ngrams", n = 3) %>% 
+    separate(words,c("word1","word2","word3"),sep = " ") %>% 
+    left_join(sentiword_ua_ru,by=c("word1"="words")) %>% 
+    left_join(sentiword_ua_ru,by=c("word2"="words")) %>% 
+    left_join(sentiword_ua_ru,by=c("word3"="words")) %>% 
+    filter(!is.na(polarity.x)) %>% 
+    mutate(polarity=polarity.x*ifelse(!is.na(polarity.y),polarity.y,1)*ifelse(!is.na(polarity.y)&!is.na(polarity),
+                                                                              polarity,1)) %>% 
+    select(id_text,id,nwords,word1, word2, word3, polarity) %>% 
     group_by(id_text) %>% 
-    summarize(nwords = sum(nwords,na.rm=T), polarity=sum(polarity, na.rm=T)/(nwords+1)) %>% 
-    mutate(polarity=(polarity - mean(polarity)) / sd(polarity))
+    summarize(nwords = mean(nwords,na.rm=T), polarity=sum(polarity, na.rm=T)/(nwords+1))
 }
 
 tidysent_by <- function(x, person){
   x %>% 
     as_data_frame() %>% 
-    mutate(id_text=c(1:length(x)))  %>%  
+    mutate(id_text=c(1:12059),nwords=stringr::str_count(value,"\\S+"))  %>%  
     unnest_tokens(sentence,value,token = "sentences") %>% 
-    mutate(nwords=stringr::str_count("\\S+")) %>%  
     group_by(id_text) %>% 
     mutate(id=c(1:n())) %>% 
-    filter(grepl(person,sentence)) %>% 
-    unnest_tokens(words,sentence) %>% 
-    left_join(sentiword_ua_ru,by="words") %>% 
+    unnest_tokens(words,sentence,token = "ngrams", n = 3) %>% 
+    filter(grepl(person,words)) %>% 
+    separate(words,c("word1","word2","word3"),sep = " ") %>% 
+    left_join(sentiword_ua_ru,by=c("word1"="words")) %>% 
+    left_join(sentiword_ua_ru,by=c("word2"="words")) %>% 
+    left_join(sentiword_ua_ru,by=c("word3"="words")) %>% 
+    filter(!is.na(polarity.x)) %>% 
+    mutate(polarity=polarity.x*ifelse(!is.na(polarity.y),polarity.y,1)*ifelse(!is.na(polarity.y)&!is.na(polarity),
+                                                                              polarity,1)) %>% 
+    select(id_text,id,nwords,word1, word2, word3, polarity) %>% 
     group_by(id_text) %>% 
-    summarize(nwords = sum(nwords,na.rm=T), polarity=sum(polarity, na.rm=T)/(nwords+1)) %>% 
-    mutate(polarity=(polarity - mean(polarity)) / sd(polarity))
+    summarize(nwords = mean(nwords,na.rm=T), polarity=sum(polarity, na.rm=T))
 }
+
+por_t <- tidysent_by(masiv$title_ru,"порошенко") %>% arrange(polarity)
 
 
