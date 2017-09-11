2c878d5c1985bf9881eb3e5214a2d8f18404b6c7
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-09-11 19:03:09 +0300

    Revert "publishing deck"
    This reverts commit 8cb0cd98a6e405b19b63d9a0b77169cd2cffb7bf.

--- elasic_connect.R
+++ elasic_connect.R
@@ -20,7 +20,7 @@ connect(es_host = "13.59.27.188", es_port = 9200)
 es <- "13.59.27.188:9200"
 
 res <- Search(index = paste0("urls_",gsub("-","",as.character(Sys.Date()-1))), body=q , 
-              type = 'news',scroll = "1m")
+              type = 'news',source = paste("title","fullhtml",sep=",") ,scroll = "1m")
 out <- res$hits$hits
 hits <- 1
 while(hits != 0){
